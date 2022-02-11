9bf8f215e7ce880602afa33efd00bdc80504c8e1
poo3fuma <coolishpoo3@gmail.com>
2022-02-11 17:17:50 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var hasValue = false;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = 'hello';
+var back = 'hello';
+console.log(hasValue);
+var person = {
+    name: "tarou",
+    age: 22
+};
+console.log(person);
---
+++ index.ts
@@ -0,0 +1,19 @@
+let hasValue = false;
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12
+let single: string = 'hello';
+let double: string = 'hello';
+let back: string = 'hello';
+
+console.log(hasValue);
+
+const person = {
+  name: "tarou",
+  age: 22
+}
+
+console.log(person)
+
+const fruits = ['Apple', 'Banana', 21]
+
