cb3a10922ce319979acc4859d354248257800a07
poo3fuma <coolishpoo3@gmail.com>
2022-02-12 15:01:00 +0900

    use type alias

--- index.ts
+++ index.ts
@@ -1,74 +1,83 @@
 let hasValue = false;
 let count: number = 10;
 let float: number = 3.14;
-let negative: number = -0.12
-let single: string = 'hello';
-let double: string = 'hello';
-let back: string = 'hello';
+let negative: number = -0.12;
+let single: string = "hello";
+let double: string = "hello";
+let back: string = "hello";
 
 console.log(hasValue);
 
 const person = {
   name: "tarou",
-  age: 22
-}
+  age: 22,
+};
 
-console.log(person)
+console.log(person);
 
-const fruits = ['Apple', 'Banana', 21]
+const fruits = ["Apple", "Banana", 21];
 
 // const book = ['business', 1500, false];
-const book: [string, number, boolean] = [
-  'business', 1500, false
-];
+const book: [string, number, boolean] = ["business", 1500, false];
 
 // use enum
-// 列挙型（3つ以上のフラグがあるときに使用する） 
+// 列挙型（3つ以上のフラグがあるときに使用する）
 
 enum CoffeeSize {
-  SHORT = 'SHORT',
-  TALL = 'TALL',
-  GRANDE = 'GRANDE',
-  VENTI = 'VENTI'
+  SHORT = "SHORT",
+  TALL = "TALL",
+  GRANDE = "GRANDE",
+  VENTI = "VENTI",
 }
 
 const coffee = {
   hot: true,
-  size: CoffeeSize.TALL
-}
+  size: CoffeeSize.TALL,
+};
 
 coffee.size = CoffeeSize.SHORT;
 
-console.log(coffee)
+console.log(coffee);
 
 // use any type
 // anyなるだけ使わないようにするのがいい
 
 let anything: any = true;
-anything = { name: 'hogehoge', age: 1000 }
+anything = { name: "hogehoge", age: 1000 };
 
-let banana = 'banana'
-banana = anything
+let banana = "banana";
+banana = anything;
 
-console.log(banana)
+console.log(banana);
 
 // use union type
 // 複数の型を受け入れることができる
 // 数字、文字列など何でもごちゃまぜで入れれる
 
-let unionType: number | string = 10
-unionType = "hello"
-unionType.toUpperCase()
+let unionType: number | string = 10;
+unionType = "hello";
+unionType.toUpperCase();
 
-console.log(unionType.toUpperCase())
+console.log(unionType.toUpperCase());
 
 // use Literal
 // 指定した値のみを格納できる変数を作成
 
-const appple: 'apple' = 'apple'
-const literal = 'literal'
+const appple: "apple" = "apple";
+const literal = "literal";
 
 // 3つの文字列のどれかしかいれられない
-let clothSize: 'small' | 'medium' | 'large' = 'large'
+let clothSize: "small" | "medium" | "large" = "large";
 
-console.log(clothSize)
+console.log(clothSize);
+
+// type aries
+type setSize = "S" | "M" | "L";
+
+const macSet: {
+  drink: setSize;
+  poteto: setSize;
+} = {
+  drink: "M",
+  poteto: "L",
+};
