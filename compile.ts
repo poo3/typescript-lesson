f8d04e4d953dba89feb6942f564ececb70baedeb
poo3fuma <coolishpoo3@gmail.com>
2022-02-13 05:58:07 +0900

    Setting compile files

--- compile.ts
+++
@@ -1,7 +0,0 @@
-// tsc -w
-// ウォッチモード（.tsファイルが変更されると自動でコンパイルする）
-
-// tsc --init
-// tsconfig.jsonファイルを自動作成できる
-
-console.log("tsconfigの色々な設定していきます");
--- tmp/compile.ts
+++ tmp/compile.ts
@@ -1,6 +1,11 @@
-"use strict";
 // tsc -w
 // ウォッチモード（.tsファイルが変更されると自動でコンパイルする）
+
 // tsc --init
 // tsconfig.jsonファイルを自動作成できる
+
 console.log("tsconfigの色々な設定していきます");
+
+// includeはコンパイルさせるファイルを指定する
+// excludeはコンパイルさせないファイルを指定する
+// filesはinclude,excludeと違いワイルドカードを使用できない
