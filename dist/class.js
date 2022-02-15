dd2a27b7f33691bc280b7046fe82850a552ba39e
poo3fuma <coolishpoo3@gmail.com>
2022-02-16 03:13:40 +0900

    Assert class

--- dist/class.js
+++ dist/class.js
@@ -4,6 +4,15 @@ class Person {
         this.name = initName;
         this.age = initAge;
     }
+    greeting() {
+        console.log(`Hello! My name is ${this.name}.My age is ${this.age}`);
+    }
 }
-const poo3 = new Person("poo3", 21);
+const poo3 = new Person("person", 21);
 console.log(poo3);
+const user = {
+    name: "poo3",
+    age: 25,
+    userGreeting: poo3.greeting,
+};
+user.userGreeting();
--- src/class.ts
+++ src/class.ts
@@ -5,7 +5,19 @@ class Person {
     this.name = initName;
     this.age = initAge;
   }
+  // class内のthisについても型宣言をしてあげる
+  greeting(this: { name: string; age: number }) {
+    console.log(`Hello! My name is ${this.name}.My age is ${this.age}`);
+  }
 }
 
-const poo3 = new Person("poo3", 21);
+const poo3 = new Person("person", 21);
 console.log(poo3);
+
+const user = {
+  name: "poo3",
+  age: 25,
+  userGreeting: poo3.greeting,
+};
+
+user.userGreeting();
